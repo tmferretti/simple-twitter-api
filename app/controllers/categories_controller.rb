e31d4a9d299052bc3565578e8c8e0437fe036a2b
Michael <michael.ferretti@outlook.com>
2018-03-27 16:53:19 -0400

    Add index and show actions to categories/tweets controllers & install jbuilder

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,10 @@
 class CategoriesController < ApplicationController
+	
+	def index
+		
+	end
+
+	def show
+		
+	end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,10 @@
 class TweetsController < ApplicationController
+
+	def index
+		
+	end
+
+	def show
+		
+	end
 end
